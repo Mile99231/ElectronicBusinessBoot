9e9cb1cf77e65e24112a1ca842066f117b7604da
Mile99231 <751550475@qq.com>
2023-07-20 13:44:27 +0800

    Initial commit

---
+++ src/main/java/com/zretc/ElectronicBusinessBoot/ElectronicBusinessBootApplication.java
@@ -0,0 +1,13 @@
+package com.zretc.ElectronicBusinessBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicBusinessBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicBusinessBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zretc/ElectronicBusinessBoot/ElectronicBusinessBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zretc.ElectronicBusinessBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicBusinessBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
